c977e38bad7ca88121dea48aba96d41d96111059
Malnen <46851853+Malnen@users.noreply.github.com>
2021-09-05 13:00:50 +0200

    początki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SpotifyService} from './login/service/spotify.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ SpotifyService]
 })
 export class AppComponent {
   title = 'Tunefy';
+
 }
