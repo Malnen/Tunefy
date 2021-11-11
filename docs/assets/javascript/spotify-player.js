a2eac0d7fc221843bfbb503ef7ca7dacfe57faeb
Malnen <46851853+Malnen@users.noreply.github.com>
2021-11-11 14:59:48 +0100

    docs

---
+++ docs/assets/javascript/spotify-player.js
@@ -0,0 +1,32 @@
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = window.localStorage.getItem('accessToken');
+    const player = new Spotify.Player({
+        name: 'Tunefy',
+        getOAuthToken: cb => {
+            cb(token)
+        }
+    });
+
+    // Error handling
+    player.addListener('initialization_error', ({message}) => {
+        console.error(message);
+    });
+    player.addListener('authentication_error', ({message}) => {
+        console.error(message);
+    });
+    player.addListener('account_error', ({message}) => {
+        console.error(message);
+    });
+    player.addListener('playback_error', ({message}) => {
+        console.error(message);
+    });
+
+    player.addListener('ready', ({device_id}) => {
+        const event = new Event('playerInitialized');
+        event.id = device_id;
+        window.dispatchEvent(event);
+        console.log('Ready with Device ID: ' + device_id);
+    });
+
+    player.connect();
+}
